2e92f07f862a7acaf9818d3c502ef661d63bcde0
GeorgeSamir10 <georgesamir3500@gmail.com>
2023-10-08 17:06:19 -0700

    task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
-/*
- * first output program
+/**
+ * main - Entry point
+ *
+ * Description: this C program Prints with put function
+ *
+ * Return: Alway 0 (Success) 
  */
-int main()
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
