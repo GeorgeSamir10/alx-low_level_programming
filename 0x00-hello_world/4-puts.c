2be36fc728874c2a41080b7f2b472b2195ba223a
george samir <141030802+GeorgeSamir10@users.noreply.github.com>
2023-10-09 03:09:40 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
-/*
- * main - Entry point
- *
- * Description: This C program Prints with the put function
- *
- * Return:Always 0(Success) 
- */
+/**
+*main - Entry point
+*
+*Description: This C program Prints with the put function
+*
+*Return: Always0(Success) 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
