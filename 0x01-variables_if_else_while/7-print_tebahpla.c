721b5a7ea22d28d3535c04354d7a8cb442772ccb
GeorgeSamir10 <georgesamir3500@gmail.com>
2023-10-11 12:57:35 -0700

    t7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: a c program to print alphabet reversed
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	i = 'z';
+	while (i <= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
