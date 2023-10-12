3c675d6c418ea6e811343d7989c7bc4fea8a59fc
GeorgeSamir10 <georgesamir3500@gmail.com>
2023-10-12 12:39:29 -0700

    t1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * descrition: c program that prints lowercase alphabet using function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
