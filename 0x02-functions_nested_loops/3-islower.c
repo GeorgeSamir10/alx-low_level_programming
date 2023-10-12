6faef8b2c3405739f4d69d2f3bd56496900a9c5b
GeorgeSamir10 <georgesamir3500@gmail.com>
2023-10-12 13:52:00 -0700

    t3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 
 /**
  * _islower - c function to detect wheather a character is lowercase or not
+ * @c: inputs the character the function to determine it is Lowercase or not
  */
 
 int _islower(int c)
