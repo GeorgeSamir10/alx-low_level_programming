355f0f466262ff8c7beb2eae7dfe300d691096f8
GeorgeSamir10 <georgesamir3500@gmail.com>
2023-10-16 14:03:13 -0700

    t104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -34,7 +34,7 @@ int numLength(int num)
 int main(void)
 {
 	int count, initial;
-	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, sumo = 0;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
 
 	for (count = 1; count <= 98; count++)
 	{
@@ -59,4 +59,5 @@ int main(void)
 		else
 			printf("\n");
 	}
+	return (0);
 }
