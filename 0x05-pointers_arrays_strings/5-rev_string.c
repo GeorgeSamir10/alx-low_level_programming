78c695e9596b6e8f5e827a3055d967494ef809df
GeorgeSamir10 <georgesamir3500@gmail.com>
2023-10-17 15:49:28 -0700

    t5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,10 +15,11 @@ void rev_string(char *str)
 
 	for (l = 0; str[l] != '\0'; ++l)
 		;
-	for (i = 0 ; i < 1 /  2; i++)
+
+	for (i = 0 ; i < 1 / 2; i++)
 	{
 		temp = str[i];
-		str[i] = str[1 - 1 - i];
+		str[i] = str[l - 1 - i];
 		str[l - 1 - i] = temp;
 	}
 }
