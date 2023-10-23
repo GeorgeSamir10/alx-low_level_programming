001cf59c52d17b7ca1c1b3de8f69a327b8448c9a
GeorgeSamir10 <georgesamir3500@gmail.com>
2023-10-23 14:37:47 -0700

    t2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memcpy -copies memory slots to another
+ *
+ * @dest: memory slot
+ * @src: source slot
+ * @n: the length of the file will be copied
+ *
+ * Return: dest pointer
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int z;
+
+	for (z = 0; z < n; z++)
+	{
+		dest[z] = src[z];
+	}	
+	return (dest);
+}
