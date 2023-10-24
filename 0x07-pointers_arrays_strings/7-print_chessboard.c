54f1a033cddae0759919b95c1532f3c8ce172c06
GeorgeSamir10 <georgesamir3500@gmail.com>
2023-10-23 17:34:19 -0700

    t7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints a chessboard like code shape
+ *
+ * @a: rows input
+ *
+ * Return: 0 (Success)
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int y, z;
+
+	for (y = 0; y < 8; y++)
+	{
+		for (z = 0; z < 8; z++)
+		{
+			_putchar(a[y][z]);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
