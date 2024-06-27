3808078107d1d32d1c6cfffb35785160cd11153f
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-06-28 01:42:12 +0300

    first task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isupper - A function that checks for uppercase character.
+ *
+ * @c: is the character to  check
+ *
+ * Return: 1 if c is uppercase otherwise 0
+ */
+int _isupper(int c)
+{
+	int x = isupper(c);
+
+	if (x == 0)
+		return (x);
+	else
+		return (1);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the directory of Project "more functions, more nested loops".
