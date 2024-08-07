827dd5a304cf0ddca20ba06ed7ad95308e0769ca
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-08-07 23:39:05 +0300

    task0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This is the Structures and TypeDef Project in ALX Software Enginee
+ring Program
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - struct with name dog
+* @name: dog's name
+* @owner: dog's owner's name
+* @age: dog's age
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
