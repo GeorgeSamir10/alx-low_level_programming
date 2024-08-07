9082b2d76671e5a34f2b3323ba516127bd895167
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-08-07 04:02:51 +0300

    task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description: program to print the current file
+* Return: 0 (Success)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
