ae20b3294ab05b18e5ad90a936655a2122925e3e
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-09-12 00:32:34 +0300

    TASK3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,4 +7,28 @@
 * Return: 1 on success and -1 on failure
 */
 
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd1, fd2, size;
 
+	if (!filename)
+		return (-1);
+
+	fd1 = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd1 == -1)
+		return (-1);
+
+	for (size = 0; text_content[size]; size++)
+		;
+
+	if (text_content != NULL)
+	{
+		fd2 = write(fd1, text_content, size);
+		if (fd2 == -1)
+			return (-1);
+	}
+
+	close(fd1);
+	return (1);
+}
