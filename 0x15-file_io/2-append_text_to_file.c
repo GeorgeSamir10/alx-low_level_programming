688350081624029de22d788070ac0c9b89ebfc9c
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-09-12 00:35:50 +0300

    TASK3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd1 = open(filename, O_WRONLY | O_APPEND, 0600);
+	fd1 = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd1 == -1)
 		return (-1);
