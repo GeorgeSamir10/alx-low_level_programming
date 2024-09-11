6216f7c5cc9ee175e02ef5c0db90d386251b5c02
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-09-11 20:43:07 +0300

    Task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+* create_file - creates a new file and writes content to it
+* @filename: the name of the file to create
+* @text_content: the content of the created file
+* Return: 1 if Success, -1 if Failure.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t size = strlen(text_content);
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+
+	if (fd == -1)
+	{
+		perror("fails");
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		if (write(fd, "", 0) == -1)
+		{
+			perror("write");
+			close(fd);
+			return (-1);
+		}
+	}
+	else
+	{
+		if (write(fd, text_content, size) != size)
+		{
+			perror("write");
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -11,5 +12,6 @@
 int _putchar(char);
 int _puts(char *);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
