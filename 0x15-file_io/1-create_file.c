05b31c6be367823bde372f5aee942c2aeb62671d
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-09-11 21:32:48 +0300

    edit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,14 +9,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, fd1;
-	int size;
-	mode_t mode = S_IRUSR | S_IWUSR;
+	int fd, fd1, size;
+	/* mode_t mode = S_IRUSR | S_IWUSR; */
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
