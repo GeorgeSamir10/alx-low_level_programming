8f7b10169b4412c29aac8644fd7473384ef28a18
GeorgeSamir10 <georgesamir3500@gmail.com>
2024-09-12 00:38:17 +0300

    TASK3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,10 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	/*
+	*if (!text_content)
+	*	text_content = "";
+		*/
 
 	for (size = 0; text_content[size]; size++)
 		;
